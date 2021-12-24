aaeef8f0331e5999bd0d4eecb8b0b2d7f33382e0
Evgenii <Shtyrev.evgenii@gmail.com>
2021-12-24 16:33:00 +0700

    Add Mask and check text inputs

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import modals from './modules/modals';
 import slider from './modules/slider';
 import forms from './modules/forms';
+import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -10,5 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
     slider('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     slider('.main-slider-item', 'vertical');
     forms();
-
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
 });
